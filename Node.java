167166f44e1b936efe61c5ccb86822a25f03cf3d
luthfiyaindah11 <90297490+luthfiyaindah11@users.noreply.github.com>
2023-05-26 07:56:34 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
